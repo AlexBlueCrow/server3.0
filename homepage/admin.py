edeeee9bf19d7046eca7aa4fc9dc926e4c4ca48f
havefunzheng <forstarcraft213@163.com>
2020-08-10 16:19:23 +0800

    del

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,5 +1,5 @@
 from django.contrib  import admin
-from .models import StaticFiles,AdminUser,VideoFiles,PicFiles,VIMap,Key
+from .models import AdminUser,VideoFiles,PicFiles,VIMap,Key
 # Register your models here.
 
-admin.site.register([StaticFiles,AdminUser,VideoFiles,PicFiles,VIMap,Key])
+admin.site.register([AdminUser,VideoFiles,PicFiles,VIMap,Key])
