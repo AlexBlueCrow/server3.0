4faff9182b71b7ab53faf36d3b4108eaa4212a23
havefunzheng <forstarcraft213@163.com>
2020-08-14 09:26:25 +0800

    good

--- homepage/tools.py
+++ homepage/tools.py
@@ -4,6 +4,6 @@ from .serializers import KeySerializer
 def getKeys():
     key = Key.objects.get(account='qj')
     key_ser = KeySerializer(key,many=False)
-    print(key_ser.data,key_ser.data['appid'])
+    
 
     return key_ser.data
