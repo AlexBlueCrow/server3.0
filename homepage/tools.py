5ce58efec3d94c31a506081c8f850bd8d64ccf02
havefunzheng <forstarcraft213@163.com>
2020-08-14 08:38:34 +0800

    rename

--- homepage/tools.py
+++ homepage/tools.py
@@ -4,5 +4,6 @@ from .serializers import KeySerializer
 def getKeys():
     key = Key.objects.get(account='qj')
     key_ser = KeySerializer(key,many=False)
-    print(key_ser)
-    return key_ser
+    print(key_ser.data,key_ser.data['appid'])
+
+    return key_ser.data
