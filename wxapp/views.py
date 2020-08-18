5f7e2899339881803b20029baf21e21c418f4534
havefunzheng <forstarcraft213@163.com>
2020-08-18 17:59:40 +0800

    ses_check

--- wxapp/views.py
+++ wxapp/views.py
@@ -197,8 +197,8 @@ def comment_post(request):
     SensCheckUrl = 'https://api.weixin.qq.com/wxa/msg_sec_check?access_token='+accToken
     data = {'content': comment_text}
 
-    data_json = json.dumps(data,ensure_ascii=True)
-    data = json.loads(data_json,encoding='utf-8')
+    # data_json = json.dumps(data,ensure_ascii=True)
+    # data = json.loads(data_json,encoding='utf-8')
     r = json.loads(requests.post(
         SensCheckUrl, data=json.dumps(data).encode('utf-8')))
     print('---r----',r)
