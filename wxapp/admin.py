a7159a4ad96a6eb68153ef784f64081e6f3a1825
havefunzheng <forstarcraft213@163.com>
2020-10-20 17:51:55 +0800

    model.itemshadow

--- wxapp/admin.py
+++ wxapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import AppUser,Item,Order,Comments,Prepay_Order,Varify_failed,Captain,FarmUser,Sell,Adopt,Text
+from .models import AppUser,Item,Order,Comments,Prepay_Order,Varify_failed,Captain,FarmUser,Sell,Adopt,Text,ItemShadow
 # Register your models here.
 
-admin.site.register([AppUser,Item,Order,Comments,Prepay_Order,Varify_failed,Captain,FarmUser,Sell,Adopt,Text])
+admin.site.register([AppUser,Item,Order,Comments,Prepay_Order,Varify_failed,Captain,FarmUser,Sell,Adopt,Text,ItemShadow])
 
